92fa54d98863f4ed748d6c85c96900db929101c7
Ehsan Hossein Khani <149901492+DiakoCompany@users.noreply.github.com>
2023-11-29 00:12:55 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,61 +1,96 @@
+function load()
+{
+    TabChange('Home');
+}
 function TabChange(TabName)
 {
-     var Home = document.getElementById('Home');
-     var Biography = document.getElementById('Biography');
-     var Records = document.getElementById('Records');
-     var Cammping = document.getElementById('Cammping');
-     var Contact = document.getElementById('Contact');
+     var Home_Tab = document.getElementById('Home-Tab');
+     var Biography_Tab = document.getElementById('Biography-Tab');
+     var Records_Tab = document.getElementById('Records-Tab');
+     var Cammping_Tab = document.getElementById('Cammping-Tab');
+     var Contact_Tab = document.getElementById('Contact-Tab');
+    //  ------------------------------------------------------
+    var Home = document.getElementById('Home');
+    var Biography = document.getElementById('Biography');
+    var Records = document.getElementById('Records');
+    var Cammping = document.getElementById('Cammping');
+    var Contact = document.getElementById('Contact');
 
     switch (TabName)
     {
         case 'Home' :
             {
-                Home.classList.add('UnderLine');
-                Biography.classList.remove('UnderLine');
-                Records.classList.remove('UnderLine');
-                Cammping.classList.remove('UnderLine');
-                Contact.classList.remove('UnderLine');
+                Home_Tab.classList.add('UnderLine');
+                Biography_Tab.classList.remove('UnderLine');
+                Records_Tab.classList.remove('UnderLine');
+                Cammping_Tab.classList.remove('UnderLine');
+                Contact_Tab.classList.remove('UnderLine');
                 // -----------------------------------
+                Home.style.display = "flex";
+                Biography.style.display = "none";
+                Records.style.display = "none";
+                Cammping.style.display = "none";
+                Contact.style.display = "none";
                 break;
             }
         case 'Biography' :
             {
-                Home.classList.remove('UnderLine');
-                Biography.classList.add('UnderLine');
-                Records.classList.remove('UnderLine');
-                Cammping.classList.remove('UnderLine');
-                Contact.classList.remove('UnderLine');
+                Home_Tab.classList.remove('UnderLine');
+                Biography_Tab.classList.add('UnderLine');
+                Records_Tab.classList.remove('UnderLine');
+                Cammping_Tab.classList.remove('UnderLine');
+                Contact_Tab.classList.remove('UnderLine');
                 // -----------------------------------
+                Home.style.display = "none";
+                Biography.style.display = "flex";
+                Records.style.display = "none";
+                Cammping.style.display = "none";
+                Contact.style.display = "none";
                 break;
             }
         case 'Records' :
             {
-                Home.classList.remove('UnderLine');
-                Biography.classList.remove('UnderLine');
-                Records.classList.add('UnderLine');
-                Cammping.classList.remove('UnderLine');
-                Contact.classList.remove('UnderLine');
+                Home_Tab.classList.remove('UnderLine');
+                Biography_Tab.classList.remove('UnderLine');
+                Records_Tab.classList.add('UnderLine');
+                Cammping_Tab.classList.remove('UnderLine');
+                Contact_Tab.classList.remove('UnderLine');
                 // -----------------------------------
+                Home.style.display = "none";
+                Biography.style.display = "none";
+                Records.style.display = "flex";
+                Cammping.style.display = "none";
+                Contact.style.display = "none";
                 break;
             }
         case 'Cammping' :
             {
-                Home.classList.remove('UnderLine');
-                Biography.classList.remove('UnderLine');
-                Records.classList.remove('UnderLine');
-                Cammping.classList.add('UnderLine');
-                Contact.classList.remove('UnderLine');
+                Home_Tab.classList.remove('UnderLine');
+                Biography_Tab.classList.remove('UnderLine');
+                Records_Tab.classList.remove('UnderLine');
+                Cammping_Tab.classList.add('UnderLine');
+                Contact_Tab.classList.remove('UnderLine');
                 // -----------------------------------
+                Home.style.display = "none";
+                Biography.style.display = "none";
+                Records.style.display = "none";
+                Cammping.style.display = "flex";
+                Contact.style.display = "none";
                 break;
             }
         case 'Contact' :
             {
-                Home.classList.remove('UnderLine');
-                Biography.classList.remove('UnderLine');
-                Records.classList.remove('UnderLine');
-                Cammping.classList.remove('UnderLine');
-                Contact.classList.add('UnderLine');
+                Home_Tab.classList.remove('UnderLine');
+                Biography_Tab.classList.remove('UnderLine');
+                Records_Tab.classList.remove('UnderLine');
+                Cammping_Tab.classList.remove('UnderLine');
+                Contact_Tab.classList.add('UnderLine');
                 // -----------------------------------
+                Home.style.display = "none";
+                Biography.style.display = "none";
+                Records.style.display = "none";
+                Cammping.style.display = "none";
+                Contact.style.display = "flex";
                 break;
             }
     }
